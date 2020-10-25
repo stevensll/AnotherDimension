7116ce29a8f04b4409f80406910bb5962b1db314
Steven <deltaorb637@gmail.com>
2020-10-25 16:36:07 -0400

    Added and successfully tested sum() method.

--- Tester.java
+++ Tester.java
@@ -1,6 +1,12 @@
 import java.util.Arrays;
 public class Tester {
     public static void main(String[] args){
+        /* tests for sum()
+        int [] a = new int[] {1,2,3,4,5,};
+        int [] b = new int[] {};
+        System.out.println(ArrayOps.sum(a)); // 15
+        System.out.println(ArrayOps.sum(b)); // 0
+        */
     }
 }
 
